ac1d4224acac86352c20df8a84762e3176fa3943
UnsureSherlock <kevinholmes88@gmail.com>
2017-04-08 11:09:08 -0400

    Added movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
+    private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
+
 
     void Update()
     {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 20.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 20.0f;
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+        rb.MovePosition(new Vector2(rb.transform.position.x + x, rb.transform.position.y + y));
+        // transform.Move(transform.position.x + x, transform.position.y + y, transform.position.z);
     }
 }
