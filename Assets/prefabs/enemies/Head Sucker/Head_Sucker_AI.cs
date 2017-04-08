89b757d4ce1348296c4cdcf264e519d11bd47e0b
Darin Beaudreau <darinbeaudreau@gmail.com>
2017-04-08 12:38:10 -0400

    Created player and head sucker prefabs.

---
+++ Assets/prefabs/enemies/Head Sucker/Head_Sucker_AI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Head_Sucker_AI : MonoBehaviour {
+	private BoxCollider2D bc;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start() {
+		bc = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
