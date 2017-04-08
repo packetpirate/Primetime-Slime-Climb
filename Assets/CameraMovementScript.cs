d8972ede04ff82f1415efa5833757bd8cc71ea58
UnsureSherlock <kevinholmes88@gmail.com>
2017-04-08 13:33:35 -0400

    Added in background and more effects

---
+++ Assets/CameraMovementScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementScript : MonoBehaviour {
+
+
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // LateUpdate is called after Update each frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
     private Rigidbody2D rb;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
-	}
-
+    }
 
     void Update()
     {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 20.0f;
         var y = Input.GetAxis("Vertical") * Time.deltaTime * 20.0f;
 
-        rb.MovePosition(new Vector2(rb.transform.position.x + x, rb.transform.position.y + y));
-        // transform.Move(transform.position.x + x, transform.position.y + y, transform.position.z);
+        var newPosition = new Vector2(rb.transform.position.x + x, rb.transform.position.y + y);
+        rb.MovePosition(newPosition);
     }
 }
